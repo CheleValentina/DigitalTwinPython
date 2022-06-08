8658fe6389df6bcfa7c34b8f66b0182df5f46386
Valentina <chelevalentina@yahoo.com>
2022-06-08 16:49:13 +0300

    Remove unnecessary print

--- weather_api.py
+++ weather_api.py
@@ -10,5 +10,4 @@ def get_data_for_city(city: str):
     response = requests.get(settings.WEATHER_API_URL.format(city=city))
     data = json.loads(response.text)["current"]
 
-    pprint(data)
     return {"wind_mps": data["wind_kph"] * 5 / 18, "wind_degree": data["wind_degree"]}
